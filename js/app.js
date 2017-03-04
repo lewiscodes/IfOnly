8639a8866211a80364577eb92898ce78b36b1d97
Lewis <lewis.turner@Lewiss-Air.lan>
2017-03-04 18:20:53 +0000

    added random background load and gitignore

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 randomiseBackground();
 
 function randomiseBackground() {
-  console.log("helooo");
+  var randomNumber = Math.floor(Math.random() * 5) + 1;
+  var image = new Image();
+  image.onload = function() {
+    window.document.body.background = image.src;
+    window.document.body.className += "backgroundBody";
+  }
+  image.src = "./img/" + randomNumber + ".jpg";
 }
