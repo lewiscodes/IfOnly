8bb1770fca8efa1e2d70193210601c706c2e70c2
Lewis <lewis.turner@Lewiss-Air.lan>
2017-03-12 14:22:41 +0000

    combined js and added leap year validation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+// https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json
+// https://www.quandl.com/api/v3/datasets/GOOG/LON_TSCO.json
+var URL = "https://www.quandl.com/api/v3/datasets/";
+var API_KEY = "api_key=LZ5yVTJ1qD8WbKyh2GRg";
+var numberOfShares = 0;
+var eachSharehNowWorth = 0;
+var ifOnly = 0;
+var inTextWidth = $(".in").width();
+var stockTextWidth = $(".stock").width();
+var dateTextWidth = $(".date").width();
+
 var dateOptions = {
 	data: dates,
   theme: "dark",
@@ -16,6 +27,7 @@ var dateOptions = {
 		}
 	}
 };
+
 var stockOptions = {
     data: stock,
     getValue: "name",
@@ -41,9 +53,6 @@ var stockOptions = {
 			}
     }
 };
-var inTextWidth = $(".in").width();
-var stockTextWidth = $(".stock").width();
-var dateTextWidth = $(".date").width();
 
 randomiseBackground();
 $(".date").easyAutocomplete(dateOptions);
@@ -66,6 +75,14 @@ $("input.date").keyup(function() {
 	calculateResizeInputs(dateTextWidth, "date", true);
 });
 
+$("input.date").blur(function() {
+	$("input.date").val(checkLeapYear($("input.date").val()));
+});
+
+$("button").click(function() {
+	getData("2015-01-01", "2016-01-01", "WIKI/AAPL");
+});
+
 function randomiseBackground() {
   var randomNumber = Math.floor(Math.random() * 5) + 1;
   var image = new Image();
@@ -76,8 +93,18 @@ function randomiseBackground() {
   image.src = "./img/" + randomNumber + ".jpg";
 }
 
-function checkLeapYear() {
-  // todo
+function checkLeapYear(input) {
+	var date = input.slice(0, -5);
+	if (date.toUpperCase() === "29TH FEBRUARY") {
+		var year = input.substr(input.length - 4)
+		var isLeapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+
+		if (isLeapYear) {
+			return input;
+		} else {
+			return "28th February " + year;
+		}
+	}
 }
 
 function formatCurrency(inputText) {
@@ -110,4 +137,35 @@ function calculateResizeInputs(originalTextWidth, className, additionalElement) 
 			$($(visibleClass).parent()).css("width", originalTextWidth);
 		}
   }
+}
+
+function getData(startDate, endDate, code) {
+  $.getJSON(URL + code + ".json?" + API_KEY + "&start_date=" + startDate + "&end_date=" + endDate, function(data) {
+    numberOfShares = howManyShares(1000, data);
+    eachSharehNowWorth = whatAreTheyWorthNow(numberOfShares, data);
+    ifOnly = formatCurrency(Math.floor(numberOfShares * eachSharehNowWorth));
+    $(".ifOnly").text(ifOnly);
+  })
+}
+
+function howManyShares(amountInvested, shares) {
+  var openingPrice = shares.dataset.data[0][1];
+  return Math.floor(amountInvested / openingPrice);
+}
+
+function whatAreTheyWorthNow(numberofShares ,shares) {
+  var lastIndex = shares.dataset.data.length;
+  return shares.dataset.data[lastIndex - 1][3];
+}
+
+function validateCurrency(input) {
+  console.log(input);
+}
+
+function validateDate(input) {
+  console.log(input);
+}
+
+function validateTicker(input) {
+  console.log(input);
 }
